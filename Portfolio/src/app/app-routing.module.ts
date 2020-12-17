e979a6f72bf8ffea84dd410efafbccf4ee71862b
gustavonunez <guscostaf@gmail.com>
2020-12-17 20:06:42 -0300

    Nuevo portfolio

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { AboutMeComponent } from './about-me/about-me.component'
+import { MyProjectsComponent } from './my-projects/my-projects.component'
+import { DevelopmentToolsComponent } from './development-tools/development-tools.component'
+import { AcademicHistoryComponent } from './academic-history/academic-history.component'
+import { ContactComponent } from './contact/contact.component'
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about_me', component: AboutMeComponent},
+  { path: 'my_projects', component: MyProjectsComponent},
+  { path: 'development_tools', component: DevelopmentToolsComponent},
+  { path: 'academic_history', component: AcademicHistoryComponent},
+  { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AcademicHistoryComponent } from './academic-history/academic-history.component';
+import { ContactComponent } from './contact/contact.component';
+import { DevelopmentToolsComponent } from './development-tools/development-tools.component';
+import { HomeComponent } from './home/home.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    AcademicHistoryComponent,
+    ContactComponent,
+    DevelopmentToolsComponent,
+    HomeComponent,
+    MyProjectsComponent
   ],
   imports: [
     BrowserModule,
