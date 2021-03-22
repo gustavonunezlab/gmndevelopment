6d4e0dd4e200c3f9f26cb7b940d6885d726e4834
gustavonunez <guscostaf@gmail.com>
2021-03-22 06:21:35 -0300

    Formulario de contacto (interfaz)

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +24,8 @@ import { MyProjectsComponent } from './my-projects/my-projects.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- Portfolio/src/app/contact/contact.component.ts
+++ Portfolio/src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,13 +9,37 @@ import { Location } from '@angular/common';
 })
 export class ContactComponent implements OnInit {
 
+  contacto!: FormGroup;
+  submitted = false;
+
   constructor(
     private location: Location,
+    private formBuilder: FormBuilder,
+
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.contacto = new FormGroup({
+      nya: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      asunto: new FormControl('', Validators.required),
+      mensaje: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
   }
-  
+
+  get f() { return this.contacto.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contacto.invalid) {
+      return;
+    }
+
+    alert('Mensaje Enviado !')
+    this.goBack();
+  }
+
   goBack(): void {
     this.location.back();
   }
